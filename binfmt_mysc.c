dac487a27000fc97a6473a76d5038e4affb042b9
Mark Mossberg <mark.mossberg@gmail.com>
2016-01-23 17:03:15 -0500

    idk well it's not crashing anymore

--- binfmt_mysc.c
+++ binfmt_mysc.c
@@ -4,17 +4,39 @@
 #include <linux/kobject.h>
 #include <linux/list.h>
 #include <linux/slab.h>
+#include <linux/ctype.h>
 
 MODULE_LICENSE("GPL");
 
 static struct kobject *mysc_kobj;
 struct binfmt {
-    char magic[256];
-    char interp[256];
+    unsigned char magic[256];
+    unsigned char interp[256];
     struct list_head list;
 };
 
 LIST_HEAD(binfmts);
+
+static void dump(unsigned char *buf) {
+    while (*buf) {
+        printk(KERN_INFO "%x", *buf++);
+    }
+    printk(KERN_INFO "\n");
+}
+
+static size_t print_bf_struct(const int i, char *const buf, struct binfmt *bf)
+{
+    size_t bytes_written = 0;
+    bytes_written += scnprintf(buf, PAGE_SIZE, "%d: ", i);
+    unsigned char *m = bf->magic;
+    while (m) {
+        bytes_written += scnprintf(buf+bytes_written, PAGE_SIZE-bytes_written, "%x", *m++);
+        if (bytes_written == PAGE_SIZE-1)
+            return bytes_written;
+    }
+    bytes_written += scnprintf(buf+bytes_written, PAGE_SIZE-bytes_written, "%s\n", bf->interp);
+    return bytes_written;
+}
 
 static ssize_t mysc_show(struct kobject *kobj, struct kobj_attribute *attr,
         char *buf)
@@ -25,27 +47,58 @@ static ssize_t mysc_show(struct kobject *kobj, struct kobj_attribute *attr,
     struct binfmt *bf;
     int i = 0;
     list_for_each_entry(bf, &binfmts, list) {
-        bytes_written += scnprintf(curr+bytes_written, PAGE_SIZE-bytes_written, "%d: %s %s\n", i, bf->magic, bf->interp);
+        bytes_written += print_bf_struct(i, curr, bf);
         if (bytes_written == PAGE_SIZE-1)
             return bytes_written;
         printk(KERN_INFO "yo %d bf->str: %s\n", i, bf->interp);
         i++;
     }
     return bytes_written;
-    /* return scnprintf(buf, PAGE_SIZE, "hello world ugh\n"); */
 }
 
-static ssize_t hexpairs_to_buf(char *const buf, size_t bufsz, const char *hexpairs, size_t hexsz)
+static ssize_t hexpairs_to_buf(unsigned char *const buf, size_t bufsz, char *const hexpairs, size_t hexsz)
 {
-    strcpy(buf, "SPYM\x00");
-    return 0;
+    /* strcpy(buf, "SPYM\x00"); */
+    /* return 0; */
     // TODO actually implement. "aabbccdd" => \xaa\xbb\xcc\xdd
-    /* if (hexsz % 2) */
-    /*     return -EINVAL; */
-    /* for (size_t i = 0; i < hexsz; i+= 2) { */
+    unsigned char *b = buf;
+    size_t i;
+    printk(KERN_INFO "i am in hexpairs %s\n", hexpairs);
+    if (hexsz % 2)
+        goto err;
 
-    /* } */
+    printk(KERN_INFO "first pass bufsz %u hexsz %u hexpairs %s\n", bufsz, hexsz, hexpairs);
+    for (i = 0; i < hexsz; i++) {
+        if(!isxdigit(hexpairs[i])) {
+            printk(KERN_INFO "YO IT WASNT X %c\n", hexpairs[i]);
+            goto err;
+        }
+        if (hexpairs[i] >= 'A' && hexpairs[i] <= 'F') {
+                hexpairs[i] += 32; // convert all caps to lowercase
+        }
+    }
 
+    printk(KERN_INFO "second pass bufsz %u hexsz %u\n", bufsz, hexsz);
+    for (i = 0; i < hexsz; i+=2) {
+        char newchar;
+        if(isdigit(hexpairs[i]))
+            newchar = (hexpairs[i]-'0')*0x10;
+        else
+            newchar = (hexpairs[i]-'W')*0x10;
+        if(isdigit(hexpairs[i+1]))
+            newchar += (hexpairs[i+1]-'0');
+        else
+            newchar += (hexpairs[i+1]-'W');
+        *b++ = newchar;
+        printk(KERN_INFO "FUCK %x\n", (unsigned char)newchar);
+    }
+    *b = '\0';
+
+    return b-buf+1;
+
+
+err:
+    return -EINVAL;
 }
 
 static ssize_t mysc_store(struct kobject *dev, struct kobj_attribute *attr,
@@ -65,14 +118,22 @@ static ssize_t mysc_store(struct kobject *dev, struct kobj_attribute *attr,
         return -EINVAL;
     }
 
-    hexpairs_to_buf(bf->magic, sizeof(bf->magic), buf, colon - buf);
+    printk(KERN_INFO "ABOUT TO CALL HEXPAIRS %s %u %u\n", buf, colon - buf, sizeof(bf->magic));
+    ssize_t ret = hexpairs_to_buf(bf->magic, sizeof(bf->magic), buf, colon - buf);
+    if (ret < 0)
+        return ret;
     int interpsz = scnprintf(bf->interp, sizeof(bf->interp), "%s", colon+1);
     if (bf->interp[interpsz-1] == '\n') {
         bf->interp[interpsz-1] = '\0';
     }
 
 
-    printk(KERN_INFO "bf->magic: %s\n", bf->magic);
+    printk(KERN_INFO "bf->magic: ");
+    dump(bf->magic);
+    /* unsigned char *shit = bf->magic; */
+    /* while (shit) { */
+        /* printk(KERN_INFO "%x", *bf->magic); */
+    /* } */
     printk(KERN_INFO "bf->intepr: %s\n", bf->interp);
 
     list_add_tail(&bf->list, &binfmts);
@@ -111,10 +172,16 @@ static int __init init_mysc_binfmt(void)
         return ret;
 
 
-
-
-    /* register_binfmt(&spym_format); */
     return ret;
+}
+
+static void destroy_list(struct list_head *head)
+{
+    struct binfmt *pos, *next;
+    list_for_each_entry_safe(pos, next, head, list) {
+        list_del(&pos->list);
+        kfree(pos);
+    }
 }
 
 
@@ -122,8 +189,8 @@ static void exit_mysc_binfmt(void)
 {
     printk(KERN_INFO "Unregistering mysc format!\n");
     kobject_put(mysc_kobj);
+    destroy_list(&binfmts);
     // TODO free memory
-    /* unregister_binfmt(&spym_format); */
 }
 
 module_init(init_mysc_binfmt);
